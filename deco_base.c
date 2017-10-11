48b96efced22a16017398e48f538bc6357e4a6a4
aurelien9 <aure.flout@hotmail.fr>
2017-10-11 11:57:19 -0300

    correction deco_base

--- deco_base.c
+++ deco_base.c
@@ -30,6 +30,7 @@ int main(int argc, char* argv[])
 	FILE *pf_salida;
 	t_datos datos;
 	status_t st;
+	struct tm date;
 
 	if ((st = validar_argumentos(argc, argv, &pf_entrada, 1, "r")) != ST_OK)
 	{
@@ -42,7 +43,10 @@ int main(int argc, char* argv[])
 
 	while(fread(&datos, sizeof(t_datos), 1, pf_entrada))
 	{
-		fprintf(pf_salida, "%lu,%s,%s,%s,%s,%.0f,%lu\n", datos.id, datos.nombre, datos.autor, datos.genero, ctime(&datos.date), datos.puntaje, datos.resenas);
+		date = *gmtime(&datos.date);
+		date.tm_year += 1900;
+		date.tm_mon += 1;
+		fprintf(pf_salida, "%lu,%s,%s,%s,%i-%i-%i,%.0f,%lu\n", datos.id, datos.nombre, datos.autor, datos.genero, date.tm_year, date.tm_mon, date.tm_mday, datos.puntaje, datos.resenas);
 	}
 
 
