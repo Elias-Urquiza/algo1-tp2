6e09018cb325436c76ec844fe51c3faf8afdf345
Elías <24704187+GenghiSkeleton@users.noreply.github.com>
2017-10-14 16:14:13 -0300

    pequeña correccion

--- funciones.h
+++ funciones.h
@@ -5,7 +5,7 @@
 
 /*crear*/
 
-status_t validar_argumentos(int argc, char* argv[], FILE **file, int numero, char* mode);
+status_t validar_argumentos_crear(int argc, char* argv[], FILE **file, int numero, char* mode);
 t_datos convertir_datos(char** arreglo);
 char* strdup(const char* sc);
 status_t destruir_arreglo_cadenas(char** campos, size_t size);
